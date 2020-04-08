a35a9c023ffce9616a23a7bd1bf5e133ca9c7dad
John Walley <john@walley.org.uk>
2020-04-08 11:56:13 +0100

    fix: blade layout and animation

--- src/BladesContainer.js
+++ src/BladesContainer.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
-import posed, { PoseGroup } from 'react-pose';
-import Blade from 'react-rowing-blades';
+import React from "react";
+import styled from "styled-components";
+import posed, { PoseGroup } from "react-pose";
+import Blade from "react-rowing-blades";
 
 const BladeContainer = styled.ul`
+  flex: 1 1 auto;
   list-style: none;
   align-items: center;
   justify-content: center;
@@ -17,9 +18,9 @@ const BladeContainer = styled.ul`
 
 const Item = posed.li({
   pressable: true,
-  normal: { filter: 'grayscale(0%) blur(0px)', scale: 1 },
-  correct: { filter: 'grayscale(0%) blur(0px)', scale: 1.6 },
-  wrong: { filter: 'grayscale(100%) blur(10px)', scale: 1 },
+  normal: { filter: "grayscale(0%) blur(0px)", scale: 1 },
+  correct: { filter: "grayscale(0%) blur(0px)", scale: 1.6 },
+  wrong: { filter: "grayscale(100%) blur(10px)", scale: 1 },
   init: { scale: 1 },
   press: { scale: 1.2 },
   preEnter: {
@@ -30,7 +31,7 @@ const Item = posed.li({
     y: 0,
     opacity: 1,
     transition: {
-      y: { type: 'spring', stiffness: 1000, damping: 15 },
+      y: { type: "spring", stiffness: 1000, damping: 15 },
       default: { duration: 100 },
     },
   },
@@ -43,14 +44,13 @@ const Item = posed.li({
 const StyledItem = styled(Item)`
   display: block;
   width: 18vw;
-  height: 100%;
-  margin: 10px 10px 0px 10px;
+  margin: 10px 10px 0px 0px;
+  transform-origin: 50% 50% !important;
   -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
 
   @media (orientation: portrait) {
     width: 38vw;
-    height: 100%;
-    margin: 10px;
+    margin: 10px 10px 10px 0px;
   }
 `;
 
@@ -71,7 +71,7 @@ const BladesContainer = ({
           key={animated ? Math.floor(Math.random() * 1000000) : i}
           onClick={() => onClick(id)}
           pose={
-            correct === null ? 'normal' : correct === id ? 'correct' : 'wrong'
+            correct === null ? "normal" : correct === id ? "correct" : "wrong"
           }
         >
           <StyledBlade club={id} />
